600a73059dd653e9a53f384526303ec6076fe5f9
Michael McClenaghan <mike@sideline.ca>
2010-04-05 08:47:28 -0600

    The site is now coming together.  Implemented 1kb css grid amongst other things.

--- lib/serious_helper.rb
+++ lib/serious_helper.rb
@@ -1,13 +1,18 @@
+
 class Serious
   get '/articles' do    
     @recent = Article.all(:limit => Serious.items_on_index)
     @archived = Article.all(:limit => Serious.archived_on_index, :offset => Serious.items_on_index)
     erb :articles
   end
-end
-
-class Serious::Article
-  def [] *args
-    yaml[*args]
+  
+  class Article
+    def [] *args
+      yaml[*args]
+    end
+  
+    def article_paths
+      @article_paths ||= Dir[File.join('pages', '*')].sort
+    end
   end
 end
